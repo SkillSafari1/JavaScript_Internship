d6ba94480b5a1f29d974275a13cb5596923ea165
Skill Safari <70880049+SkillSafari1@users.noreply.github.com>
2022-02-19 13:49:21 +0530

    Add files via upload

---
+++ Session_1/script.js
@@ -0,0 +1,75 @@
+// Your first program
+console.log("Hello World")
+
+// Data Types
+// String = Alphabets
+// INT - Whole Number - example 1,10, 15
+// Float - Decimal and whole number - examples 10, 15, 1.5, 2.4...
+// Boolean - True or False
+
+// Example of few Data Types
+// String - 
+console.log("Skill Safari")
+
+// INT
+console.log(39)
+
+// Float
+console.log(3.5)
+
+// Boolean - all the letters will be small case
+console.log(true)
+
+
+// Example for a variable
+
+console.log("Kiran is studying in a college")
+console.log("He is 22 years old")
+console.log("This college he studies in is ABC college")
+
+// Start of Variable
+
+var nam = "Amar"
+var age = 25
+var college = "XYZ College"
+
+console.log(nam + " is studying in a college")
+console.log("His age is " + age)
+console.log("He is studying in " + college)
+
+// var = these are storage units. can be changes or modified any number of times.
+
+var q = 10
+console.log(q)
+
+var q = 15
+console.log(q)
+
+// LET = once declared, can not be changed after that
+
+let a = 35
+console.log(a)
+// Can not be changed
+// let a = 45
+// console.log(a)
+
+let b
+
+b = "Skill Safari"
+console.log(b)
+
+// CONST - used for constants. once declared, can not be changed.You will have to declare and assign the value together
+
+
+var x = 25
+var y = 15
+
+const z = x + y
+
+console.log(z)
+
+// This will not work
+// const t
+
+// t = 100
+// console.log(t)
