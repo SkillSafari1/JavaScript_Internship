a64dcef4b116657c3f95cac613b1cb43ff7a4304
Skill Safari <70880049+SkillSafari1@users.noreply.github.com>
2022-02-21 14:05:11 +0530

    Add files via upload

---
+++ Session_2/script.js
@@ -0,0 +1,97 @@
+// Math operation on INT and FLOAT data Types
+
+// SUM
+var sum = 10 + 15
+console.log(sum)
+
+// SUB
+var sub = 15 - 10
+console.log(sub)
+
+// Multiplication - * and for Division - /
+
+// Reminder
+var remain = 3 % 2
+console.log(remain)
+
+// To perform increments
+
+var sample = 10
+var sample = sample + 1
+console.log(sample)
+
+// increment a variable by one
+sample++
+console.log(sample)
+
+sample--
+console.log(sample)
+
+// Increment or decrement of number by more numbers
+
+var sample1 = 10
+// Add 10 to sample1
+sample1 +=10
+console.log(sample1)
+
+// Subtract 10 to sample1
+
+sample1 -= 10
+console.log(sample1)
+
+// You can also multiply and divide different numbers
+
+sample1 *= 10
+console.log(sample1)
+
+sample1 /= 5
+console.log(sample1)
+
+// Some advanced math operation for INT and Float
+
+var num = 3.2
+console.log(num)
+// Simply to round the value
+console.log(Math.round(num))
+
+// To round up the value
+console.log(Math.ceil(num))
+
+// To round down the value
+console.log(Math.floor(num))
+
+// -----------------------------------------------------------
+
+// Working with strings
+
+var str = "Skill Safari"
+// To find the length of this string
+console.log(str.length)
+
+// The final words of Tony Stark is "I am Iron Man"
+
+console.log("The final words of Tony Stark is \"I am Iron Man\"")
+
+console.log("This is the first line. This is the second line")
+// To make it come in 2 different line, I need to add \n
+
+console.log("This is the first line. \nThis is the second line")
+
+console.log(str)
+
+// Indexing the string
+console.log(str[5])
+
+console.log(str.indexOf("S"))
+
+console.log(str.indexOf("Safari"))
+
+// ---------------------------------------------------------------------------
+// Boolean
+// Its true or false
+
+console.log(true)
+
+console.log(6>7)
+
+console.log((5 + 25) == 30)
